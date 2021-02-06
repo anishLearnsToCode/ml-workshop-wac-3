062568c9229d9da61c83b0ae343e7834d8e81916
anishLearnsToCode <anish_@outlook.com>
2021-02-06 15:15:34 +0530

    adds day 5

--- day_2/multiple_plots.py
+++ day_2/multiple_plots.py
@@ -10,11 +10,17 @@ def f(t):
 t1 = np.arange(0, 5, 0.1)
 t2 = np.arange(0, 5, 0.02)
 
-plt.subplot(2, 1, 1)
+plt.subplot(2, 2, 1)
 plt.plot(t1, f(t1), 'o')
-plt.plot(t2, f(t2))
+plt.plot(t2, f(t2), '-')
 
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 2)
 plt.plot(t2, np.cos(2 * np.pi * t2))
+
+plt.subplot(2, 2, 3)
+plt.plot(t1, 0 * t1)
+
+plt.subplot(2, 2, 4)
+plt.plot(t1, t1)
 
 plt.show()
